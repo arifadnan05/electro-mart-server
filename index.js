e6ca4dd4676195240befafd71f6375c03ba46054
arifadnan05 <arifahamedadnan@gmail.com>
2024-08-17 18:28:09 +0600

    redme md file added

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:5174"
+      "http://localhost:5174",
+      "https://electro-marts.netlify.app"
     ],
     credentials: true,
     optionSuccessStatus: 200,
@@ -36,7 +37,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const productsCollection = client.db('electro-mart').collection('products')
 
 
@@ -56,8 +57,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
